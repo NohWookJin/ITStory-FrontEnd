efd62d31d8b53b9e9b68546361bf38b92eb3110a
NohWookJin <jinn98@kakao.com>
2022-08-15 21:26:04 +0900

    Feat: 빈 값 입력시 반환 기능 추가

---
+++ src/components/CommentInput.tsx
@@ -0,0 +1,95 @@
+import React, { FormEvent } from 'react';
+import useComments from '../hooks/api/useComments';
+import useCommentInput from '../hooks/useCommentInput';
+
+//css
+import styled from 'styled-components';
+import { colors } from '../styles/Colors';
+
+export default function CommentInput() {
+  const { createComments } = useComments();
+  const { commentName, commentContent, onChangeCommentName, onChangeCommentContent, clearCommentInput } =
+    useCommentInput({});
+
+  function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (commentName.length == 0 || commentContent.length == 0) {
+      alert('이름 또는 내용이 비어있습니다.');
+    } else {
+      createComments(commentName, commentContent);
+      clearCommentInput();
+    }
+  }
+
+  return (
+    <Wrapper>
+      <Form onSubmit={onSubmit}>
+        <CommentWriter placeholder="이름" value={commentName} onChange={onChangeCommentName} />
+        <CommentContent placeholder="내용" value={commentContent} onChange={onChangeCommentContent} />
+        <CommentSubmit>등록</CommentSubmit>
+      </Form>
+    </Wrapper>
+  );
+}
+const Wrapper = styled.div`
+  width: 100%;
+  min-width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  padding: 0 20rem;
+  margin-bottom: 2rem;
+  @media screen and (max-width: 1150px) {
+    padding: 0 10rem;
+  }
+  @media screen and (max-width: 500px) {
+    padding: 0 5rem;
+  }
+`;
+const Form = styled.form`
+  padding-top: 1rem;
+  display: flex;
+  width: 100%;
+`;
+const CommentWriter = styled.input`
+  width: 15%;
+  height: 3vh;
+  border: none;
+  border-bottom: 0.08rem solid rgba(0, 0, 0, 0.1);
+  margin-right: 0.3rem;
+  :focus {
+    outline: none;
+  }
+  @media screen and (max-width: 1150px) {
+    width: 10%;
+  }
+  @media screen and (max-width: 500px) {
+    width: 10%;
+  }
+`;
+const CommentContent = styled.input`
+  width: 80%;
+  height: 3vh;
+  border: none;
+  border-bottom: 0.08rem solid rgba(0, 0, 0, 0.1);
+  margin-right: 0.8rem;
+  :focus {
+    outline: none;
+  }
+  @media screen and (max-width: 1150px) {
+    width: 75%;
+  }
+  @media screen and (max-width: 500px) {
+    width: 70%;
+  }
+`;
+const CommentSubmit = styled.button`
+  opacity: 0.6;
+  border: none;
+  background-color: white;
+  font-size: 0.8rem;
+  :hover {
+    opacity: 1;
+    color: ${colors.main};
+  }
+`;
