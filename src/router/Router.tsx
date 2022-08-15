9f901b4ec44040ae4f941109f60a7b9250a9cd06
NohWookJin <jinn98@kakao.com>
2022-08-15 21:22:23 +0900

    디테일 페이지 연결

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from '../App';
 import Write from '../pages/Write';
+import Detail from '../pages/Detail';
 import NotFound from '../pages/NotFound';
 
 export default function Router() {
@@ -9,7 +10,8 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/writes/" element={<Write />} />
+        <Route path="/writes" element={<Write />} />
+        <Route path="/posts/:postId" element={<Detail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
