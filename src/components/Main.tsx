b4656f68fec558a73309565819e51d7c9ee71514
NohWookJin <jinn98@kakao.com>
2022-08-15 21:06:24 +0900

    Design: 글 작성 버튼 디자인 변경

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -17,12 +17,23 @@ const Container = styled.div`
   min-width: 50vw;
   display: flex;
   justify-content: center;
-  margin-top: 4rem;
+  margin-top: 2rem;
   padding: 0 20rem;
+  @media screen and (max-width: 1150px) {
+    padding: 0 10rem;
+  }
+  @media screen and (max-width: 500px) {
+    padding: 0 5rem;
+  }
 `;
 const NewPostBtn = styled.button`
   padding: 0.7rem;
+  border: none;
   border-radius: 5%;
   color: ${colors.white};
   background-color: ${colors.main};
+  :hover {
+    font-weight: 800;
+    transition: all 0.5s;
+  }
 `;
