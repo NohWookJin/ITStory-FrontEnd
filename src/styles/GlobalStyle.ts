d7c74e7f63312040d3e595f1fa16fa1b787e8785
NohWookJin <jinn98@kakao.com>
2022-08-15 21:24:20 +0900

    Design: 색상 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -7,4 +7,15 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
         text-decoration: none;
     }
+    body {
+        font-family: "D2";
+    }
+    @font-face {
+        font-family: "HANNA";
+        src: url("../fonts/BMHANNAPro.ttf");
+    }
+    @font-face {
+        font-family: "D2";
+        src: url("../fonts/D2Coding-Ver1.3.2-20180524-all.ttc");
+    }
 `;
