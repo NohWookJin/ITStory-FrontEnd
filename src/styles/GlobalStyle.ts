4cc061f2561e0d4f47150b3b192bc1cb6ea9d29e
NohWookJin <jinn98@kakao.com>
2022-08-17 06:00:06 +0900

    Feat: 다크모드 색상 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -7,8 +7,13 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
         text-decoration: none;
         font-family: "Nanum";
+        
     }
-
+    body {
+        color: ${props => props.theme.mode.color};
+	    background-color: ${props => props.theme.mode.bgColor};
+        
+    }
     @font-face {
     font-family: "D2";
     src: url("./fonts/D2Coding-Ver1.3.2-20180524-all.ttc");
